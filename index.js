ed0625e3483f849822a7f523f1a7262a40941fc2
Ben Syverson <ben@bensyverson.com>
2016-09-17 14:26:42 -0500

    Cleanup, improving Control

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 var App = App || 										require('./lib/app.js');
 var Control = Control || 								require('./lib/control.js');
 var Representable = Representable || 					require('./lib/ishmael.js');
-//var locales = locales || 								require('./lib/i18n.js');
 var IDEvent = IDEvent || 								require('./lib/idevent.js');
 var UIButton = UIButton || 								require('./lib/uibutton.js');
 var UIColor = UIColor || 								require('./lib/uicolor.js');
@@ -15,7 +14,6 @@ module.exports = {
 	App: App,
 	Control: Control,
 	Representable: Representable,
-	//locales: locales,
 	IDEvent: IDEvent,
 	UIButton: UIButton,
 	UIColor: UIColor,
