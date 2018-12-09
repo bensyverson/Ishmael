e7dac6b4a4ef314ff83e10a42fe25204014231c3
Ben Syverson <ben@bensyverson.com>
2018-12-09 13:45:08 -0600

    Add Common to exports

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var App = App || 										require('./lib/app.js');
 var Control = Control || 								require('./lib/control.js');
+var Common = Common || 									require('./lib/common.js');
 var Representable = Representable || 					require('./lib/ishmael.js');
 var IDEvent = IDEvent || 								require('./lib/idevent.js');
 var UIButton = UIButton || 								require('./lib/uibutton.js');
@@ -13,6 +14,7 @@ var ViewController = ViewController ||					require('./lib/viewcontroller.js');
 module.exports = {
 	App: App,
 	Control: Control,
+	Common: Common,
 	Representable: Representable,
 	IDEvent: IDEvent,
 	UIButton: UIButton,
